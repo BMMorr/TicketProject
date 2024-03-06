b78350d490ac5614905e0d1388d6a10fce9e06f4
BMMorr <125929824+BMMorr@users.noreply.github.com>
2024-03-06 12:32:11 -0600

    Update TicketCategory.cs

--- TicketProject/Data/Enum/TicketCategory.cs
+++ TicketProject/Data/Enum/TicketCategory.cs
@@ -2,8 +2,11 @@
 {
     public enum TicketCategory
     {
+        [Display(Name = "CrankyApe.com")]
         CrankyApe = 1,
+        [Display(Name = "CrankyShip.com")]
         CrankyShip,
+        [Display(Name = "Trade-It-In")]
         TradeItIn
     }
 }
